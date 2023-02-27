a3011953452efa911449166ae0900506fd510b47
Naol Dereje <nad49naol@gmail.com>
2023-02-27 17:25:30 +0300

    Naol Dereje Main application started printing the version and help manual feature added

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "btf.h"
+#include "btf_types.h"
+#include "main.h"
+
+// prints the version 
+void btf_print_version(){
+	printf("%s\n", BTF_HELP);
+}
+
+// prints the help manual
+void btf_print_help(){
+	printf("%s\n", BTF_HELP);
+}
+
+int main(){
+
+}
+
---
+++ main.h
@@ -0,0 +1,2 @@
+void btf_print_version();
+void btf_print_help();
