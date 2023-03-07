c24c8221e24e342aceed3d03d95b200d25e84a0a
Naol Dereje <nad49naol@gmail.com>
2023-03-07 23:14:50 +0300

    Naol Dereje fopen function redefined

--- main.c
+++ main.c
@@ -14,7 +14,31 @@ void btf_print_help(){
 	printf("%s\n", BTF_HELP);
 }
 
-int main(int argc, char* argv[]){
+i32 myArgc;
+btf_str* myArgv;
 
+// argument operation
+btf_str btf_short_arg_opt;
+btf_str btf_long_arg_opt;
+boolean btf_arg_short_opt; // enabled if short argument is provided (-i)
+boolean btf_arg_long_opt; // enabled if long argument is provided (--info)
+
+void btf_parse_args(){
+
+}
+
+void btf_show_info(){
+	if(myArgc < NO_ARG_LEN){
+		// shows every available info 
+		// about the device hardware
+		return;
+	}
+
+}
+int kain(int argc, char* argv[]){
+	myArgc = argc;
+	myArgv = argv;
+	btf_parse_args();
+	btf_show_info();
 }
 
--- main.h
+++ main.h
@@ -1,2 +1,7 @@
+#define NO_ARG_LEN 2 
+
+void btf_parse_args();
 void btf_print_version();
 void btf_print_help();
+void btf_no_argument();
+void btf_show_info();
